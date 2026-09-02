4d7bec10472a28f476ab95fd408b9e9f9ab92c1e
พันตำรวจโทวิศิษฎ์ ทองโม้ รอง.สวป.ภาคส่วน <biodiesel.production.station@outlook.co.th>
2026-09-02 09:27:33 +0700

    Create work.go

---
+++ mark/work.go
@@ -0,0 +1,130 @@
+package mark
+
+import (
+	"bytes"
+	"slices"
+
+	"github.com/kovetskiy/mark/attachment"
+	cparser "github.com/kovetskiy/mark/parser"
+	crenderer "github.com/kovetskiy/mark/renderer"
+	"github.com/kovetskiy/mark/stdlib"
+	"github.com/kovetskiy/mark/types"
+	"github.com/reconquest/pkg/log"
+	mkDocsParser "github.com/stefanfritsch/goldmark-admonitions"
+	"github.com/yuin/goldmark"
+
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer"
+	"github.com/yuin/goldmark/renderer/html"
+	"github.com/yuin/goldmark/util"
+)
+
+// Renderer renders anchor [Node]s.
+type ConfluenceExtension struct {
+	html.Config
+	Stdlib      *stdlib.Lib
+	Path        string
+	MarkConfig  types.MarkConfig
+	Attachments []attachment.Attachment
+}
+
+// NewConfluenceRenderer creates a new instance of the ConfluenceRenderer
+func NewConfluenceExtension(stdlib *stdlib.Lib, path string, cfg types.MarkConfig) *ConfluenceExtension {
+	return &ConfluenceExtension{
+		Config:      html.NewConfig(),
+		Stdlib:      stdlib,
+		Path:        path,
+		MarkConfig:  cfg,
+		Attachments: []attachment.Attachment{},
+	}
+}
+
+func (c *ConfluenceExtension) Attach(a attachment.Attachment) {
+	c.Attachments = append(c.Attachments, a)
+}
+
+func (c *ConfluenceExtension) Extend(m goldmark.Markdown) {
+
+	m.Renderer().AddOptions(renderer.WithNodeRenderers(
+		util.Prioritized(crenderer.NewConfluenceTextRenderer(c.MarkConfig.StripNewlines), 100),
+		util.Prioritized(crenderer.NewConfluenceBlockQuoteRenderer(), 100),
+		util.Prioritized(crenderer.NewConfluenceCodeBlockRenderer(c.Stdlib, c.Path), 100),
+		util.Prioritized(crenderer.NewConfluenceFencedCodeBlockRenderer(c.Stdlib, c, c.MarkConfig), 100),
+		util.Prioritized(crenderer.NewConfluenceHTMLBlockRenderer(c.Stdlib), 100),
+		util.Prioritized(crenderer.NewConfluenceHeadingRenderer(c.MarkConfig.DropFirstH1), 100),
+		util.Prioritized(crenderer.NewConfluenceImageRenderer(c.Stdlib, c, c.Path, c.MarkConfig.ImageAlign), 100),
+		util.Prioritized(crenderer.NewConfluenceParagraphRenderer(), 100),
+		util.Prioritized(crenderer.NewConfluenceLinkRenderer(), 100),
+		util.Prioritized(crenderer.NewConfluenceTaskListRenderer(), 100),
+	))
+
+	if slices.Contains(c.MarkConfig.Features, "mkdocsadmonitions") {
+		m.Parser().AddOptions(
+			parser.WithBlockParsers(
+				util.Prioritized(mkDocsParser.NewAdmonitionParser(), 100),
+			),
+		)
+
+		m.Renderer().AddOptions(renderer.WithNodeRenderers(
+			util.Prioritized(crenderer.NewConfluenceMkDocsAdmonitionRenderer(), 100),
+		))
+	}
+
+	if slices.Contains(c.MarkConfig.Features, "mention") {
+		m.Parser().AddOptions(
+			parser.WithInlineParsers(
+				util.Prioritized(cparser.NewMentionParser(), 99),
+			),
+		)
+
+		m.Renderer().AddOptions(renderer.WithNodeRenderers(
+			util.Prioritized(crenderer.NewConfluenceMentionRenderer(c.Stdlib), 100),
+		))
+	}
+
+	m.Parser().AddOptions(parser.WithInlineParsers(
+		// Must be registered with a higher priority than goldmark's linkParser to make sure goldmark doesn't parse
+		// the <ac:*/> tags.
+		util.Prioritized(cparser.NewConfluenceTagParser(), 199),
+	))
+}
+
+func CompileMarkdown(markdown []byte, stdlib *stdlib.Lib, path string, cfg types.MarkConfig) (string, []attachment.Attachment, error) {
+	log.Tracef(nil, "rendering markdown:\n%s", string(markdown))
+
+	confluenceExtension := NewConfluenceExtension(stdlib, path, cfg)
+
+	converter := goldmark.New(
+		goldmark.WithExtensions(
+			extension.Footnote,
+			extension.DefinitionList,
+			extension.NewTable(
+				extension.WithTableCellAlignMethod(extension.TableCellAlignStyle),
+			),
+			confluenceExtension,
+			extension.GFM,
+		),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithUnsafe(),
+			html.WithXHTML(),
+		))
+
+	ctx := parser.NewContext(parser.WithIDs(&cparser.ConfluenceIDs{Values: map[string]bool{}}))
+
+	var buf bytes.Buffer
+	err := converter.Convert(markdown, &buf, parser.WithContext(ctx))
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	html := buf.Bytes()
+
+	log.Tracef(nil, "rendered markdown to html:\n%s", string(html))
+
+	return string(html), confluenceExtension.Attachments, nil
+}
